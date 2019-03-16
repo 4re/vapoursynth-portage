7a3445e81c7713a3ec3538234cb97d13063b475f
4re <surukko@gmail.com>
2019-03-16 13:10:53 +0100

    Update new_package.sh

--- Documentation/new_package.sh
+++ Documentation/new_package.sh
@@ -40,7 +40,7 @@ version_increase() {
 
 mheader() {
 cat << EOF
-# Copyright 1999-2016 Gentoo Foundation
+# Copyright 1999-2019 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=5
